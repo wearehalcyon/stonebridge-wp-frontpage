b240560810c9e5673f5aa704af5c14ae0faecb5e
unknown <thewitness45@gmail.com>
2026-07-24 18:04:56 +0300

    init

--- functions.php
+++ functions.php
@@ -1,36 +1,15 @@
 <?php
-/**
- * Register widget area.
- *
- * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
- */
-function stonebridge_widgets_init() {
-	register_sidebar(
-		array(
-			'name'          => esc_html__( 'Sidebar', 'stonebridge' ),
-			'id'            => 'sidebar-1',
-			'description'   => esc_html__( 'Add widgets here.', 'stonebridge' ),
-			'before_widget' => '<section id="%1$s" class="widget %2$s">',
-			'after_widget'  => '</section>',
-			'before_title'  => '<h2 class="widget-title">',
-			'after_title'   => '</h2>',
-		)
-	);
+// Init vendor
+if (file_exists(__DIR__ . '/vendor/autoload.php')) {
+    require_once __DIR__ . '/vendor/autoload.php';
 }
-add_action( 'widgets_init', 'stonebridge_widgets_init' );
 
-/**
- * Enqueue scripts and styles.
- */
+// Enqueue scripts and styles.
 function stonebridge_scripts() {
-	wp_enqueue_style( 'stonebridge-style', get_stylesheet_uri(), array(), _S_VERSION );
+	wp_enqueue_style( 'stonebridge-style', get_stylesheet_uri(), array(), '' );
 	wp_style_add_data( 'stonebridge-style', 'rtl', 'replace' );
 
-	wp_enqueue_script( 'stonebridge-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
-
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
+	wp_enqueue_script( 'stonebridge-app', get_template_directory_uri() . '/assets/js/app.js', array(), '', true );
 }
 add_action( 'wp_enqueue_scripts', 'stonebridge_scripts' );
 
---
+++ templates/frontpage.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Template name: Frontpage
+ */
+get_header();
+?>
+
+<?php
+get_footer();
